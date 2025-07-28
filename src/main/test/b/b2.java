e1219ae9d3d89caaffc55f8c33326dbeccb432db
plainday <aeijmn@live.com>
2025-07-28 15:23:30 +0900

    Revert "Modified b2.java and b3.java"
    This reverts commit a5ae69511e685c887e26caf6f61cb2fa30fa7408.

--- src/main/test/b/b2.java
+++ src/main/test/b/b2.java
@@ -7,6 +7,3 @@ Select Settings > General.
 Expand Visibility and access controls.
 Select the Disable personal access tokens checkbox.
 Select Save changes.
-
-Change the diff colors
-Diffs use two different background colors to show changes between versions of code. By default, the original file is in red, and the changes are in green.
--- src/main/test/b/b3.java
+++ src/main/test/b/b3.java
@@ -3,7 +3,3 @@ History
 In GitLab 15.7 and later, you can use the disable_personal_access_tokens attribute in the application settings API to disable personal access tokens.
 
 After you have used the API to disable personal access tokens, those tokens cannot be used in subsequent API calls to manage this setting. To re-enable personal access tokens, you must use the GitLab Rails console. You can also upgrade to GitLab 17.3 or later so you can use the Admin UI instead.
-
-Change the syntax highlighting theme
-History 
-Syntax highlighting is a feature in code editors and IDEs. The highlighter assigns a color to each type of code, such as strings and comments.
