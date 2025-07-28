c2a1f3eaf033704407027c5025bd21f8153259ed
plainday <aeijmn@live.com>
2025-07-28 14:08:14 +0900

    Modified b1.java

--- src/main/test/b/b1.java
+++ src/main/test/b/b1.java
@@ -1,9 +1,5 @@
 Disable personal access tokens for enterprise users
-History 
-Prerequisites:
 
-You must have the Owner role for the group that the enterprise user belongs to.
-Disabling the personal access tokens of a group’s enterprise users:
 
 Stops the enterprise users from creating new personal access tokens. This behavior applies even if an enterprise user is also an administrator of the group.
 Disables the existing personal access tokens of the enterprise users.
