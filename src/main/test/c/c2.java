7f2ffa3495332c367bc98b3517f8c9b850153002
plainday <aeijmn@live.com>
2025-07-28 14:10:55 +0900

    Modified c1.java and c2.java

--- src/main/test/c/c2.java
+++ src/main/test/c/c2.java
@@ -2,3 +2,7 @@ Access token expiration
 
 
 Personal access tokens expire on the date you define, at midnight, 00:00 AM UTC. A token with the expiration date of 2024-01-01 expires at 00:00:00 UTC on 2024-01-01.
+
+
+Behavior
+Use the Behavior section to customize the behavior of the system layout and default views. You can change your layout width and choose the default content for your homepage, group and project overview pages. You have options to customize appearance and function, like whitespace rendering, file display, and text automation.
