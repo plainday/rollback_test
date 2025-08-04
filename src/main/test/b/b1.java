c516f07a58d3fb94ecf32f961301ea0dbec3a09a
plainday <aeijmn@live.com>
2025-08-04 15:49:04 +0900

    Feature b test

--- src/main/test/b/b1.java
+++ src/main/test/b/b1.java
@@ -4,3 +4,5 @@ Disable personal access tokens for enterprise users
 Stops the enterprise users from creating new personal access tokens. This behavior applies even if an enterprise user is also an administrator of the group.
 Disables the existing personal access tokens of the enterprise users.
 Disabling personal access tokens for enterprise users does not disable personal access tokens for service accounts.
+
+Added feature b.
